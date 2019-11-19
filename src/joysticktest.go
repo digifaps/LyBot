36d6262999b3832bca15c4d33fffed0a669ec215
fabrice <fabricezander@gmail.com>
2019-11-19 22:28:26 +0100

    joysticktest drop

---
+++ src/joysticktest.go
@@ -0,0 +1,164 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"time"
+
+	MQTT "github.com/eclipse/paho.mqtt.golang"
+	"github.com/nsf/termbox-go"
+	"github.com/simulatedsimian/joystick"
+)
+
+// MQTT
+
+type controller struct {
+	axis   []int
+	button []int
+}
+
+//define a function for the default message handler
+var f MQTT.MessageHandler = func(client MQTT.Client, msg MQTT.Message) {
+	//	fmt.Printf("TOPIC: %s\n", msg.Topic())
+	//	fmt.Printf("MSG: %s\n", msg.Payload())
+}
+
+func mqtt_init() *MQTT.ClientOptions {
+	opts := MQTT.NewClientOptions().AddBroker("tcp://localhost:1883")
+	opts.SetClientID("jetson-nano")
+	opts.SetDefaultPublishHandler(f)
+	return opts
+}
+
+func mqtt_connectClient(opts *MQTT.ClientOptions) MQTT.Client {
+	c := MQTT.NewClient(opts)
+	if token := c.Connect(); token.Wait() && token.Error() != nil {
+		panic(token.Error())
+	}
+	return c
+}
+
+//Publish 5 messages to /go-mqtt/sample at qos 1 and wait for the receipt
+func mqtt_publish(c MQTT.Client, topic string, text string) {
+
+	if token := c.Subscribe(topic, 0, nil); token.Wait() && token.Error() != nil {
+		fmt.Println(token.Error())
+		os.Exit(1)
+	}
+
+	token := c.Publish(topic, 0, false, text)
+	token.Wait()
+
+	if token := c.Unsubscribe(topic); token.Wait() && token.Error() != nil {
+		fmt.Println(token.Error())
+		os.Exit(1)
+	}
+
+}
+
+func mqtt_disconnect(c MQTT.Client) {
+	//unsubscribe from /go-mqtt/sample
+	if token := c.Unsubscribe("/"); token.Wait() && token.Error() != nil {
+		fmt.Println(token.Error())
+		os.Exit(1)
+	}
+
+	c.Disconnect(250)
+}
+
+// joystick
+func printAt(x, y int, s string) {
+	for _, r := range s {
+		termbox.SetCell(x, y, r, termbox.ColorDefault, termbox.ColorDefault)
+		x++
+	}
+}
+
+func readJoystick(js joystick.Joystick, c MQTT.Client, cntl controller) {
+	jinfo, err := js.Read()
+
+	if err != nil {
+		printAt(1, 5, "Error: "+err.Error())
+		return
+	}
+
+	printAt(1, 5, "Buttons:")
+	for button := 0; button < js.ButtonCount(); button++ {
+		if jinfo.Buttons&(1<<uint32(button)) != 0 {
+			cntl.axis1 = jinfo.Buttons & (1 << uint32(button))
+			printAt(10+button, 5, "X")
+		} else {
+			printAt(10+button, 5, ".")
+		}
+	}
+
+	mqtt_publish(c, "axis5", fmt.Sprintf("Value: %7d", jinfo.AxisData[5]))
+
+	for axis := 0; axis < js.AxisCount(); axis++ {
+		printAt(1, axis+7, fmt.Sprintf("Axis %2d Value: %7d", axis, jinfo.AxisData[axis]))
+	}
+
+	return
+}
+
+func main() {
+
+	opts := mqtt_init()
+	c := mqtt_connectClient(opts)
+	var cntl *controller
+
+	jsid := 0
+	if len(os.Args) > 1 {
+		i, err := strconv.Atoi(os.Args[1])
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		jsid = i
+	}
+
+	js, jserr := joystick.Open(jsid)
+
+	if jserr != nil {
+		fmt.Println(jserr)
+		return
+	}
+
+	err := termbox.Init()
+	if err != nil {
+		panic(err)
+	}
+	defer termbox.Close()
+
+	eventQueue := make(chan termbox.Event)
+	go func() {
+		for {
+			eventQueue <- termbox.PollEvent()
+		}
+	}()
+
+	ticker := time.NewTicker(time.Millisecond * 40)
+
+	for doQuit := false; !doQuit; {
+		select {
+		case ev := <-eventQueue:
+			if ev.Type == termbox.EventKey {
+				if ev.Ch == 'q' {
+					doQuit = true
+				}
+			}
+			if ev.Type == termbox.EventResize {
+				termbox.Flush()
+			}
+
+		case <-ticker.C:
+			printAt(1, 0, "-- Press 'q' to Exit --")
+			printAt(1, 1, fmt.Sprintf("Joystick Name: %s", js.Name()))
+			printAt(1, 2, fmt.Sprintf("   Axis Count: %d", js.AxisCount()))
+			printAt(1, 3, fmt.Sprintf(" Button Count: %d", js.ButtonCount()))
+			readJoystick(js, c)
+			termbox.Flush()
+		}
+	}
+}
--- src/motor.py
+++ src/motor.py
@@ -6,8 +6,9 @@ from Adafruit_MotorHAT import Adafruit_MotorHAT, Adafruit_DCMotor
 import time
 import atexit
 
+# defines
 DEADBAND = 20
-
+DEADBAND_L = -20 
 
 # create a default object, no changes to I2C address or frequency
 mh = Adafruit_MotorHAT(addr=0x60,i2c_bus=1)
@@ -25,24 +26,26 @@ atexit.register(turnOffMotors)
 def on_message_motor_left(mosq, obj, msg):
     if int(msg.payload) > DEADBAND:
         myMotorL.setSpeed(int(msg.payload))
+        myMotorL.run(Adafruit_MotorHAT.FORWARD);
     else:
         myMotorL.run(Adafruit_MotorHAT.RELEASE);
         print("MOTOR LEFT: " + msg.topic + " " + str(msg.qos) + " " + str(msg.payload))
 
-
 def on_message_motor_right(mosq, obj, msg):
     if int(msg.payload) > DEADBAND:
         myMotorR.setSpeed(int(msg.payload))
+        myMotorR.run(Adafruit_MotorHAT.BACKWARD);
     else:
         myMotorR.run(Adafruit_MotorHAT.RELEASE);
-        print("MOTOR LEFT: " + msg.topic + " " + str(msg.qos) + " " + str(msg.payload))
-
+        print("MOTOR RIGHT: " + msg.topic + " " + str(msg.qos) + " " + str(msg.payload))
 
 def on_message(mosq, obj, msg):
     print(msg.topic + " " + str(msg.qos) + " " + str(msg.payload))
 
-
 mqttc = mqtt.Client()
+
+myMotorL = mh.getMotor(1)
+myMotorR = mh.getMotor(2)
 
 # Add message callbacks that will only trigger on a specific subscription match.
 mqttc.message_callback_add("$SYS/motor/left/#", on_message_motor_left)
@@ -53,19 +56,15 @@ mqttc.subscribe("$SYS/#", 0)
 
 mqttc.loop_forever()
 
-################################# DC motor test!
-myMotorL = mh.getMotor(1)
-myMotorR = mh.getMotor(2)
-
 # set the speed to start, from 0 (off) to 255 (max speed)
-myMotorL.setSpeed(150)
-myMotorR.setSpeed(150)
-myMotorL.run(Adafruit_MotorHAT.FORWARD);
-myMotorR.run(Adafruit_MotorHAT.BACKWARD);
-time.sleep(1)
-# turn off motor
-myMotorL.run(Adafruit_MotorHAT.RELEASE);
-myMotorR.run(Adafruit_MotorHAT.RELEASE);
+# myMotorL.setSpeed(150)
+# myMotorR.setSpeed(150)
+# myMotorL.run(Adafruit_MotorHAT.FORWARD);
+# myMotorR.run(Adafruit_MotorHAT.BACKWARD);
+# time.sleep(1)
+# # turn off motor
+# myMotorL.run(Adafruit_MotorHAT.RELEASE);
+# myMotorR.run(Adafruit_MotorHAT.RELEASE);
 
-client.loop_forever()
+# client.loop_forever()
 
